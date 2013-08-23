921099f8e124258e0a82b75da17f29f534504a8f
SchautDollar <ryanschaut@schautdollar.com>
2013-08-23 13:20:10 -0500

    Changed and worked on the command "ClearInventory". It wasn't very safe. Made it console proof!

--- src/com/schautdollar/DonorDreams/Commands/CmdClearInventory.java
+++ src/com/schautdollar/DonorDreams/Commands/CmdClearInventory.java
@@ -18,6 +18,7 @@ public class CmdClearInventory implements ICommand {
 	
 	final String COMMAND = "clearinv";
 	final String PERMISSION_NODE = "donordreams.clearinv";
+	final String PERMISSION_NODE_OTHERS = "donordreams.clearinv.others";
 	
 	
 	@Override
@@ -31,25 +32,38 @@ public class CmdClearInventory implements ICommand {
 	}
 
 	@Override
-	public boolean runCommand(CommandSender sender, Command cmd, String lbl,
-			String[] args) {
-		
-		Player player = (Player) sender;
-		
-		if(cmd.getName().equalsIgnoreCase(this.COMMAND) && DonorDreams.permManager.playerHasPerm(player, this.PERMISSION_NODE)){
-			
-			player.getInventory().clear();
-			
-			
-		}else{
-			player.sendMessage(ChatColor.RED + "You do not have permission to do this.");
-			return true;
+	public boolean runCommand(CommandSender sender, Command cmd, String lbl, String[] args) {
+		if(cmd.getName().equalsIgnoreCase(COMMAND)) {
+			if(sender instanceof Player) {
+				Player player = (Player) sender;
+				if(DonorDreams.permManager.playerHasPerm(player, this.PERMISSION_NODE)){
+					if(args.length == 0){
+						player.getInventory().clear();
+						player.sendMessage(ChatColor.GRAY + "Cleared!");
+						return true;
+					
+					}else if(args.length == 1 && DonorDreams.permManager.playerHasPerm(player, PERMISSION_NODE_OTHERS)){
+						Player targetPlayer = player.getServer().getPlayer(args[0]);
+						targetPlayer.getInventory().clear();
+						targetPlayer.sendMessage(ChatColor.GRAY + "Your inventory has been cleared!");
+						return true;
+					}else{
+						player.sendMessage(ChatColor.RED + "You do not have permission to clear others inventories.");
+						return true;
+					}
+				}
+				else{
+					player.sendMessage(ChatColor.RED + "You do not have permission to clear your inventory.");
+					return true;
+				}
+			}
+			else{
+				//implement TARGETED PLAYER
+				DonorDreams.logMessage(ChatColor.RED + "Silly console. The command \"" + this.getCommand() + "\" can only be used by a player.");
+				return true;
+			}
 		}
-		
-		
-		
-		
-		return false;
+		else
+			return false;
 	}
-
 }
